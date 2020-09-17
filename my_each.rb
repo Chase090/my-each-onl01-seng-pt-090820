6d5a0d3b62916e69c53c9ab1a1158f754a829587
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-17 19:33:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 require 'pry'
 def my_each(collection) # put argument(s) here
@@ -9,3 +10,13 @@ def my_each(collection) # put argument(s) here
   return collection
 end
 binding.pry
+=======
+def my_each(collection) # put argument(s) here
+  counter = 0
+  while counter < collection.length
+   yield (collection[counter])
+   counter += 1
+  end
+  return collection
+end
+>>>>>>> a1e9cc5d81951e9a42fd5369e860f93dc840f065
