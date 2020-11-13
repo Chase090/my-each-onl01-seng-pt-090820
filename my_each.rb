e87ecbf541f15b7f1296c6100a9518a6010c6469
Chase090 <carloyam21@icloud.com>
2020-11-12 19:03:53 -0800

    commit tree 4ce412656e887cabb43b3ead51d5b39ab0ec606d

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,13 @@
 require 'pry'
 def my_each(collection) # put argument(s) here
   counter = 0
+ 
   while counter < collection.length
    yield (collection[counter])
    counter += 1
+    puts "this is #{collection}"
   end
-  return collection
+ 
+   
 end
 binding.pry
